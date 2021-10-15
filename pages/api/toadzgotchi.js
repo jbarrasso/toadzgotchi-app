9d98b0e7cb56569f54160cf9c6d8c333cb0e7320
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2021-10-15 00:59:08 -0700

    Added hardhat and ethers dependencies; Added MetaMask integration

--- pages/api/toadzgotchi.js
+++ pages/api/toadzgotchi.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ethers } from "ethers";
 
 export default async function initToadzgotchi() {
+  
   let feedButton = document.getElementById("feedButton");
   let playButton = document.getElementById("playButton");
   let restButton = document.getElementById("sleepButton");
@@ -8,6 +10,13 @@ export default async function initToadzgotchi() {
   feedButton.addEventListener("click", setHungerBar)
   playButton.addEventListener("click", setMoodBar)
   restButton.addEventListener("click", setRestBar)
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  // Prompt user for account connections
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+  console.log("Account:", await signer.getAddress());
+  
 }
 
 async function setHungerBar() {
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import initToadzgotchi from './api/toadzgotchi';
+import initToadzgotchi from './api/toadzgotchi'
 
 export default function Home() {
-  initToadzgotchi();
-  return (
+    initToadzgotchi();
+    return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
