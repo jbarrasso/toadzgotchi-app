72ebf402b4b4ae04645fbd222490ed365f342bb4
jbarrasso <jeffb7@bu.edu>
2021-12-27 22:58:25 -0500

    Remove rinkeby from hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,11 +40,6 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    rinkeby: {
-      url: process.env.RINKEBY_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
     hardhat: {
       chainId: 1337
     },
