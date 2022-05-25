aebb9a2c5bea6f0e9791b45de05159fcb517193c
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2022-05-24 19:01:22 -0700

    remove headers from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/:path*{/}?',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-          {
-            key: 'Content-Type',
-            value: 'application/json',
-          },
-        ],
-      },
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:path*{/}?',
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: '*',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Credentials',
+  //           value: 'true',
+  //         },
+  //         {
+  //           key: 'Content-Type',
+  //           value: 'application/json',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   images: {
     domains: ['ipfs.io'],
   },
