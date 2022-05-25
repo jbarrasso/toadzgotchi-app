d25c1c98a1eb3b11abe089873a7f89c5cf872434
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2022-05-24 19:31:24 -0700

    add context to getserversideprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,8 +37,13 @@ export let welcomeMessages = ['Welcome back to the swamp!',
 
 //Runs on the server, not client
 export async function getServerSideProps(context) {
+  context.res.setHeader('Content-Type', ['application/json', 'charset=utf-8'])
+  context.res.setHeader("Access-Control-Allow-Origin", "*")
+  context.res.setHeader("Access-Control-Allow-Credentials", true)
+
   const allToadz = await prisma.toadz.findMany()
   const allOwners = await prisma.user.findMany()
+  
   return {
     props: {
       toadData: allToadz,
