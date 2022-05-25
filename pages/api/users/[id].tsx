a4ea9559c8c3c3eba1d840229919aed458dbd320
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2022-05-25 15:43:13 -0700

    test

--- pages/api/users/[id].tsx
+++ pages/api/users/[id].tsx
@@ -24,7 +24,7 @@ async function queryToadContract(account: string) {
 export default async function getUserByAddress(req:NextApiRequest, res:NextApiResponse) {
     res.statusCode = 200;
     const account = req.body
-    res.send('hi')
+    res.json({message:'hi'})
     //Double check the requester (account) owns toadz or not
     const toadIdsOwned = await queryToadContract(account)
 
