f1a4f5521b323da6f871dfef97dbf3b85ea2b14b
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2022-06-02 14:21:25 -0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -241,7 +241,7 @@ function Home({toadData, ownerData}) {
   }
 
   async function updateOwner(account: string) {
-    const res = await fetch(`https://${process.env.VERCEL_URL}/api/users/` + account, {
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/users/` + account, {
       method: 'PUT',
       body: JSON.stringify(account)
     })
