cc27671df21e2c4d9a4ade4049e44b6e57fc9818
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2022-06-05 19:20:48 -0700

    syntax

--- pages/index.tsx
+++ pages/index.tsx
@@ -287,7 +287,7 @@ function Home({toadData, ownerData}) {
   }
 
   async function updateStats(properties: string[]) {
-    const res = await fetch('/api/toadstats', {
+    const res = await fetch('/api/toadStats', {
       method: 'PATCH',
       body: JSON.stringify(properties)
     })
