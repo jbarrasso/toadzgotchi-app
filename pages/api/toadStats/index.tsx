84489e7069bafcb0254f8e99ef3cf09ea3a32972
Jeffrey Barrasso <jeffreybarrasso@gmail.com>
2022-06-13 20:45:17 -0700

    test

--- pages/api/toadStats/index.tsx
+++ pages/api/toadStats/index.tsx
@@ -422,7 +422,7 @@ export default async function getToadById( req:NextApiRequest, res:NextApiRespon
                     const callGithubAction = await fetch(`https://api.github.com/repos/jbarrasso/toadzgotchi-app/dispatches`, {
                         method: 'POST',
                         headers: {
-                                    Authorization: `token ${token}`,
+                                    Authorization: `Bearer ${token}`,
                                     Accept : "application/vnd.github.v3+json"
                                 },
                         body: JSON.stringify({
